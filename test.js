cfc68bb943f76a2820a5942a0a587338e867ee8b
Titus Wormer <tituswormer@gmail.com>
2018-04-16 21:11:35 +0200

    Refactor code-style

--- test.js
+++ test.js
@@ -1,20 +1,20 @@
-'use strict';
+'use strict'
 
-var test = require('tape');
-var bail = require('.');
+var test = require('tape')
+var bail = require('.')
 
-test('bail([err])', function (t) {
-  t.doesNotThrow(function () {
-    bail();
-  });
+test('bail([err])', function(t) {
+  t.doesNotThrow(function() {
+    bail()
+  })
 
-  t.throws(function () {
-    bail('foo');
-  }, /foo/);
+  t.throws(function() {
+    bail('foo')
+  }, /foo/)
 
-  t.throws(function () {
-    bail(new Error('bar'));
-  }, /bar/);
+  t.throws(function() {
+    bail(new Error('bar'))
+  }, /bar/)
 
-  t.end();
-});
+  t.end()
+})
