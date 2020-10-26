c39c9879904e9c34d73ae642ff6f2c3f515eb6e3
Titus Wormer <tituswormer@gmail.com>
2020-10-26 20:14:07 +0100

    Update dev-dependencies

--- test.js
+++ test.js
@@ -3,16 +3,16 @@
 var test = require('tape')
 var bail = require('.')
 
-test('bail([err])', function(t) {
-  t.doesNotThrow(function() {
+test('bail([err])', function (t) {
+  t.doesNotThrow(function () {
     bail()
   })
 
-  t.throws(function() {
+  t.throws(function () {
     bail('foo')
   }, /foo/)
 
-  t.throws(function() {
+  t.throws(function () {
     bail(new Error('bar'))
   }, /bar/)
 
