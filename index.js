3438bb5dcffa21141d1d312096487dd5ac3543a1
Titus Wormer <tituswormer@gmail.com>
2021-03-26 10:05:22 +0100

    Fix types

--- index.js
+++ index.js
@@ -1,21 +1,10 @@
-// prettier-ignore
 /**
  * Throw a given error.
  *
- * @type {{
- *   (error: Error): never
- *   (error: null | undefined): void
- *   (): void
- * }} */
-export var bail = (
-  /**
-   * Throw a given error.
-   *
-   * @param {Error?} [error]
-   */
-  function (error) {
-    if (error) {
-      throw error
-    }
+ * @param {Error | null | undefined} [error]
+ */
+export function bail(error) {
+  if (error) {
+    throw error
   }
-)
+}
